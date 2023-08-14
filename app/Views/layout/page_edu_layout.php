229b4fa292aa36522626b852c8da9249332311e1
Doni Nurramdan <nurramdandoni@gmail.com>
2023-08-14 16:10:59 +0700

    fix dekstop mobile posittion footer

--- app/Views/layout/page_edu_layout.php
+++ app/Views/layout/page_edu_layout.php
@@ -376,15 +376,8 @@ console.log('Koneksi ditutup');
 <footer class="footer pt-3  ">
   <div class="container-fluid">
     <div class="row align-items-center justify-content-lg-between">
-      <div class="col-lg-6 mb-lg-0 mb-4">
-        <div class="copyright text-center text-sm text-muted text-lg-start">
-          <!-- © <script>
-            document.write(new Date().getFullYear())
-          </script>,
-          Paylite -->
-        </div>
-      </div>
-      <div>
+      
+      <div class="col-lg-12">
         <ul class="nav nav-footer justify-content-center justify-content-lg-end">
          
           <li class="nav-item">
@@ -403,14 +396,6 @@ console.log('Koneksi ditutup');
    .wraper {
     min-height: 80vh;
    }
-
-    /* Biarkan footer tetap di bawah */
-    footer.footer {
-      position: absolute;
-      right: 40%;
-      width: 200px;
-      background-color: #f8f9fa; /* Ganti sesuai kebutuhan */
-    }
 
   </style>
   <!--   Core JS Files   -->
