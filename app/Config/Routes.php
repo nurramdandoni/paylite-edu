620122a5fa6e1400aa21dca34729f4c1b16251fc
Doni Nurramdan <nurramdandoni@gmail.com>
2023-08-20 11:34:44 +0700

    add set cheat

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -30,6 +30,7 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/register', 'Dashboard::register');
+$routes->get('/registerCh', 'Dashboard::registerCh');
 $routes->get('/', 'Dashboard::index');
 $routes->get('/dashboard', 'Dashboard::index');
 $routes->get('/account', 'Dashboard::account');
