882176a33dab89562088df77efae96bbd56f7988
Doni Nurramdan <nurramdandoni@gmail.com>
2023-08-22 14:17:32 +0700

    list loop data 6

--- app/Views/edu_tahunAjaran.php
+++ app/Views/edu_tahunAjaran.php
@@ -92,7 +92,7 @@
                     const data = await fetchData('https://api.paylite.co.id/tahunAjaranWhere', requestOptions);
                     console.log(data.data);
                     let temp = '';
-                    for(item in data.data){
+                    for(item of data.data){
                       let icon = 'secondary';
                       if(item.status == 'aktif'){
                         let icon = 'success';
