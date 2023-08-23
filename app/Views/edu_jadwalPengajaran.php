4c963c0f1bd39cd2f80cdd477913f9e846c8a732
Doni Nurramdan <nurramdandoni@gmail.com>
2023-08-24 01:34:00 +0700

    restore

--- app/Views/edu_jadwalPengajaran.php
+++ app/Views/edu_jadwalPengajaran.php
@@ -157,12 +157,9 @@
                     }else{
                       
                       if(jCount[item].jam_mulai != jCount[item-1].jam_mulai){
-                        if(jCount[item].nama_hari == jCount[item-1].nama_hari){
-
-                          tempCondition2 = `<td style="text-align:center;" rowspan="`+hari[jCount[item].nama_hari].jam_group.data[jCount[item].jam_mulai].data.length+`">
-                        <p class="text-xs font-weight-bold mb-0">`+jCount[item].jam_mulai+`</p>
-                      </td>`;
-                        }
+                            tempCondition2 = `<td style="text-align:center;" rowspan="`+hari[jCount[item].nama_hari].jam_group.data[jCount[item].jam_mulai].data.length+`">
+                          <p class="text-xs font-weight-bold mb-0">`+jCount[item].jam_mulai+`</p>
+                        </td>`;
                           }
                     }
                       }
