e6aa705eb3f322bb458d53947c7717dc67cd22cd
Doni Nurramdan <nurramdandoni@gmail.com>
2023-08-23 15:00:44 +0700

    mapping jadwal pushing12

--- app/Views/edu_jadwalPengajaran.php
+++ app/Views/edu_jadwalPengajaran.php
@@ -130,10 +130,10 @@
                         if(data.data[start].jam_mulai == data.data[start].jam_mulai){
                           // hari[data.data[start].nama_hari].jam_group.count++;
                           // const vari = {`${data.data[start].jam_mulai}`:"hehhe"};
-                          const vari = {start:"nyeleneh"};
-                          hari[data.data[start].nama_hari].jam_group.data.push(vari);
+                          // const vari = {start:"nyeleneh"};
+                          const datajamgroup = {nama_kelas:data.data[start].nama_kelas,nama_guru:data.data[start].nama_guru};
+                          hari[data.data[start].nama_hari].jam_group.data[data.data[start].jam_mulai].push(datajamgroup);
                           
-                          // {nama_kelas:data.data[start].nama_kelas,nama_guru:data.data[start].nama_guru}
                         }
                       }
                     }
