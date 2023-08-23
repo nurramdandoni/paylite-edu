81bfcfc9d08d164e1209f2b044df74cab1e5b8ee
Doni Nurramdan <nurramdandoni@gmail.com>
2023-08-24 06:00:45 +0700

    add api check nisn scanner player11

--- app/Views/edu_absensi.php
+++ app/Views/edu_absensi.php
@@ -128,6 +128,10 @@
   let hariIni = '';
   let Nisn = '';
   let Waktu = '';
+
+  var html5QrcodeScanner = new Html5QrcodeScanner(
+        "reader", { fps: 10, qrbox: 250 });
+    html5QrcodeScanner.render(onScanSuccess);
     function onScanSuccess(decodedText, decodedResult) {
     // Handle on success condition with the decoded text or result.
     console.log(`Scan result:`, decodedResult);
@@ -210,9 +214,7 @@
                     // }
                 }
 
-    var html5QrcodeScanner = new Html5QrcodeScanner(
-        "reader", { fps: 10, qrbox: 250 });
-    html5QrcodeScanner.render(onScanSuccess);
+    
 </script>
 <style>
     #reader {
