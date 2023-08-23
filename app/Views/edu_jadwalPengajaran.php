f9c9a330340b7ee783b4a854f438794a123d9af2
Doni Nurramdan <nurramdandoni@gmail.com>
2023-08-24 01:15:39 +0700

    style aligment text rowspan test row again try4

--- app/Views/edu_jadwalPengajaran.php
+++ app/Views/edu_jadwalPengajaran.php
@@ -154,12 +154,13 @@
                             <span class="badge badge-sm bg-gradient-success">`+jCount[item].nama_hari+`</span>
                           </div>
                       </td>`;
-                    }
+                    }else{
                       if(jCount[item].jam_mulai != jCount[item-1].jam_mulai){
                             tempCondition2 = `<td style="text-align:center;" rowspan="`+hari[jCount[item].nama_hari].jam_group.data[jCount[item].jam_mulai].data.length+`">
                           <p class="text-xs font-weight-bold mb-0">`+jCount[item].jam_mulai+`</p>
                         </td>`;
                           }
+                    }
                       }
                       temp += `
                       <tr>
