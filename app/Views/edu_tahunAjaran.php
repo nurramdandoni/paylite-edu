929c5c7cf86a6ba62a296d0dcd42784b0961c579
Doni Nurramdan <nurramdandoni@gmail.com>
2023-08-27 05:37:53 +0700

    move button

--- app/Views/edu_tahunAjaran.php
+++ app/Views/edu_tahunAjaran.php
@@ -8,12 +8,12 @@
               <h6>Tahun Ajaran</h6>
             </div>
             <div class="card-body px-0 pt-0 pb-2">
+              <!-- Button trigger modal -->
+              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                Tambah Tahun Ajaran
+              </button>
               <div class="table-responsive p-0">
 
-                <!-- Button trigger modal -->
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                  Tambah Tahun Ajaran
-                </button>
 
                 <table id="example" class="table align-items-center mb-0">
                   <thead>
