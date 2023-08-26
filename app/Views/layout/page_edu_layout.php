b50c50f23441f929599cd68ed58cc69bfdc91265
Doni Nurramdan <nurramdandoni@gmail.com>
2023-08-27 05:52:50 +0700

    modal Dinamic Contxt

--- app/Views/layout/page_edu_layout.php
+++ app/Views/layout/page_edu_layout.php
@@ -396,17 +396,17 @@ async function fetchData(url, options) {
       <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <h5 class="modal-title" id="exampleModalLabel" id="modalTitle">-</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-          <div class="modal-body">
+          <div class="modal-body" id="modalContent">
             ...
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+            <button type="button" class="btn btn-primary" id="modalButtonAction">-</button>
           </div>
         </div>
       </div>
