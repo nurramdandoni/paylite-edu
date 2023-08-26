f2698e3fa69ad826cd91dd3d24f9acd8f92ae1ea
Doni Nurramdan <nurramdandoni@gmail.com>
2023-08-27 05:33:22 +0700

    dependencies js

--- app/Views/layout/page_edu_layout.php
+++ app/Views/layout/page_edu_layout.php
@@ -17,6 +17,7 @@
 
   <!-- Bootstrap core CSS -->
   <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- new -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
   <!-- Custom styles for this template -->
@@ -448,7 +449,11 @@ async function fetchData(url, options) {
   <script src="<?= base_url(); ?>assets/js/core/bootstrap.min.js"></script>
   <script src="<?= base_url(); ?>assets/js/plugins/perfect-scrollbar.min.js"></script>
   <script src="<?= base_url(); ?>assets/js/plugins/smooth-scrollbar.min.js"></script>
-  
+
+  <!-- mew -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> 
   
   <script>
     var win = navigator.platform.indexOf('Win') > -1;
