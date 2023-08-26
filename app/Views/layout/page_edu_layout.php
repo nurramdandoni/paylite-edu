001f35a20f6e97f108eb8e210ce876b6d18db793
Doni Nurramdan <nurramdandoni@gmail.com>
2023-08-27 05:31:40 +0700

    dependencies css

--- app/Views/layout/page_edu_layout.php
+++ app/Views/layout/page_edu_layout.php
@@ -17,6 +17,7 @@
 
   <!-- Bootstrap core CSS -->
   <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
   <!-- Custom styles for this template -->
   <!-- <link href="signin.css" rel="stylesheet"> -->
