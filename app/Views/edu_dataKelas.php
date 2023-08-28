020ffcbe1ec4235e23a70c85a479e8695ed63fa1
Doni Nurramdan <nurramdandoni@gmail.com>
2023-08-29 02:38:15 +0700

    add select2 init6

--- app/Views/edu_dataKelas.php
+++ app/Views/edu_dataKelas.php
@@ -45,6 +45,7 @@
             margin-top: 10px;
         }
       </style>
+      
       <script>
         getDataKelas();
         
@@ -363,4 +364,6 @@
             previousLink.textContent = 'Prev';
         }
       </script>
+      <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+      <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <?= $this->endSection() ?>
