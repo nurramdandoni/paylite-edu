f6b0a56826920435ff2740f9dbbc1ab0a88430aa
Doni Nurramdan <nurramdandoni@gmail.com>
2023-08-29 01:10:50 +0700

    recover 1

--- app/Views/edu_dataKelas.php
+++ app/Views/edu_dataKelas.php
@@ -16,7 +16,6 @@
                       <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nama Kelas</th>
                       <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Status</th>
                       <th class="text-secondary opacity-7"></th>
-                      <th class="text-secondary opacity-7"></th>
                     </tr>
                   </thead>
                   <tbody id="list">
@@ -99,11 +98,6 @@
                       </td>
                       <td class="align-middle text-center text-sm">
                         <span class="badge badge-sm bg-gradient-`+icon+`">Aktif</span>
-                      </td>
-                      <td class="align-middle">
-                        <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
-                          Edit
-                        </a>
                       </td>
                       <td class="align-middle">
                         <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
