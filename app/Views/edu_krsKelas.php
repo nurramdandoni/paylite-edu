653c8e544a55cc90e10c8581f8d09b3dd9fb076b
Doni Nurramdan <nurramdandoni@gmail.com>
2023-08-30 15:17:47 +0700

    add remove krs container

--- app/Views/edu_krsKelas.php
+++ app/Views/edu_krsKelas.php
@@ -239,7 +239,7 @@
                     console.log("folower", data.data);
                     let temp2 = '';
                     for(item2 of data.data){
-                      temp2 += `<div style="width:100%;" class="btn btn-success">`+item2.siswa.nisn+" - "+item2.siswa.nama_siswa+`<div class="btn btn-danger">X</div></div>`;
+                      temp2 += `<div><div class="btn btn-success">`+item2.siswa.nisn+" - "+item2.siswa.nama_siswa+`</div><div class="btn btn-danger">X</div></div>`;
                     }
                     $("#modalTitle").html("Follower di "+item2.kelas.nama_kelas+" Tahun Ajaran "+item2.tahun_ajaran.nama_tahun_ajaran+"");
                     $("#lsKrsFollow").html(temp2);
