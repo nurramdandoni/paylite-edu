50610612e027332b1c3081ba12cad455ef484d90
Doni Nurramdan <nurramdandoni@gmail.com>
2023-09-04 05:41:19 +0700

    view

--- app/Controllers/PdfController.php
+++ app/Controllers/PdfController.php
@@ -10,28 +10,29 @@ class PdfController extends BaseController
 {
     public function generatePDF()
     {
-        return view('pdf_template_absensi');
+        // return view('pdf_template_absensi');
         // // Buat objek DOMPDF
-        // $dompdf = new Dompdf();
+        $dompdf = new Dompdf();
 
         // // Buat opsi DOMPDF
-        // $options = new Options();
-        // $options->set('isHtml5ParserEnabled', true);
-        // $options->set('isPhpEnabled', true);
+        $options = new Options();
+        $options->set('isHtml5ParserEnabled', true);
+        $options->set('isPhpEnabled', true);
 
         // // Terapkan opsi ke objek DOMPDF
-        // $dompdf->setOptions($options);
+        $dompdf->setOptions($options);
 
         // // // Isi konten PDF (ganti dengan konten laporan Anda)
-        // $html = view('pdf_template_absensi'); // Misalnya, gunakan view untuk mengatur tampilan laporan
+        $html = view('pdf_template_absensi'); // Misalnya, gunakan view untuk mengatur tampilan laporan
 
         // // Muat konten ke DOMPDF
-        // $dompdf->loadHtml($html);
+        $dompdf->loadHtml($html);
 
         // // Render PDF (menghasilkan PDF)
-        // $dompdf->render();
+        $dompdf->render();
 
         // // Tampilkan atau unduh PDF
         // $dompdf->stream("laporan_absensi.pdf", array("Attachment" => false));
+        $dompdf->stream();
     }
 }
