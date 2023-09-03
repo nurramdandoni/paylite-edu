6faf4bf38cc73ad8758793ca73c20a0e2c3fcea4
Doni Nurramdan <nurramdandoni@gmail.com>
2023-09-04 05:46:06 +0700

    add setpaper

--- app/Controllers/PdfController.php
+++ app/Controllers/PdfController.php
@@ -4,27 +4,16 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use Dompdf\Dompdf;
-use Dompdf\Options;
 
 class PdfController extends BaseController
 {
     public function generatePDF()
     {
-        // return view('pdf_template_absensi');
-        // // Buat objek DOMPDF
         $dompdf = new Dompdf();
-
-        // // Buat opsi DOMPDF
-        $options = new Options();
-        $options->set('isHtml5ParserEnabled', true);
-        $options->set('isPhpEnabled', true);
-
-        // // Terapkan opsi ke objek DOMPDF
-        $dompdf->setOptions($options);
 
         // // // Isi konten PDF (ganti dengan konten laporan Anda)
         $html = view('pdf_template_absensi'); // Misalnya, gunakan view untuk mengatur tampilan laporan
-
+        $dompdf->setPaper('A4','portrait');
         // // Muat konten ke DOMPDF
         $dompdf->loadHtml($html);
 
