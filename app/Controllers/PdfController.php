8e9a6428f31194adaad760ed0ec30e2d8e47011b
Doni Nurramdan <nurramdandoni@gmail.com>
2023-09-04 05:16:16 +0700

    add otion set

--- app/Controllers/PdfController.php
+++ app/Controllers/PdfController.php
@@ -14,13 +14,13 @@ class PdfController extends Controller
         // Buat objek DOMPDF
         $dompdf = new Dompdf();
 
-        // // Buat opsi DOMPDF
-        // $options = new Options();
-        // $options->set('isHtml5ParserEnabled', true);
-        // $options->set('isPhpEnabled', true);
+        // Buat opsi DOMPDF
+        $options = new Options();
+        $options->set('isHtml5ParserEnabled', true);
+        $options->set('isPhpEnabled', true);
 
-        // // Terapkan opsi ke objek DOMPDF
-        // $dompdf->setOptions($options);
+        // Terapkan opsi ke objek DOMPDF
+        $dompdf->setOptions($options);
 
         // // Isi konten PDF (ganti dengan konten laporan Anda)
         // $html = view('pdf_template_absensi'); // Misalnya, gunakan view untuk mengatur tampilan laporan
