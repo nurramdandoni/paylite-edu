178bbbf0003e1a3bd2577d07ad92e07fe1d160f8
Doni Nurramdan <nurramdandoni@gmail.com>
2023-09-04 05:36:53 +0700

    add BaseCOntroller

--- app/Controllers/PdfController.php
+++ app/Controllers/PdfController.php
@@ -6,21 +6,20 @@ use Dompdf\Dompdf;
 use Dompdf\Options;
 use CodeIgniter\Controller;
 
-class PdfController extends Controller
+class PdfController extends BaseController
 {
     public function generatePDF()
     {
-        echo "test";
         // Buat objek DOMPDF
         $dompdf = new Dompdf();
 
         // Buat opsi DOMPDF
-        // $options = new Options();
-        // $options->set('isHtml5ParserEnabled', true);
-        // $options->set('isPhpEnabled', true);
+        $options = new Options();
+        $options->set('isHtml5ParserEnabled', true);
+        $options->set('isPhpEnabled', true);
 
-        // // Terapkan opsi ke objek DOMPDF
-        // $dompdf->setOptions($options);
+        // Terapkan opsi ke objek DOMPDF
+        $dompdf->setOptions($options);
 
         // // Isi konten PDF (ganti dengan konten laporan Anda)
         $html = view('pdf_template_absensi'); // Misalnya, gunakan view untuk mengatur tampilan laporan
