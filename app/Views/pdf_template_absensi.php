51186cc87eb8ec4ebbd9da8754017ef51bc36bd5
Doni Nurramdan <nurramdandoni@gmail.com>
2023-09-04 10:23:44 +0700

    generate header blanko1

--- app/Views/pdf_template_absensi.php
+++ app/Views/pdf_template_absensi.php
@@ -7,7 +7,7 @@
         /* CSS untuk mengatur tampilan header */
         .header {
             display: flex;
-            justify-content: space-between;
+            justify-content: center;
             align-items: center;
             border-bottom: 2px solid #000;
             padding: 10px 0;
@@ -28,6 +28,9 @@
             padding: 10px 0;
             text-align: center;
         }
+        .header1 {
+            font-size: 20px;
+        }
     </style>
 </head>
 <body>
@@ -37,10 +40,10 @@
         <img class="header-logo" src="https://edu.paylite.co.id/assets/img/logo_main.jpeg" alt="Logo">
         <!-- Informasi Header -->
         <div class="header-info">
-            <div>PEMERINTAH KABUPATEN KUNINGAN</div>
-            <div>DINAS PENDIDIKAN DAN KEBUDAYAAN</div>
-            <div>SEKOLAH DASAR NEGERI 3 HAURKUNING</div>
-            <div>Dusun Kaliwon, Kecamatan Nusaherang, Kabupaten Kuningan, Jawa Barat</div>
+            <div class="header1">PEMERINTAH KABUPATEN KUNINGAN</div>
+            <div class="header2">DINAS PENDIDIKAN DAN KEBUDAYAAN</div>
+            <div class="header3">SEKOLAH DASAR NEGERI 3 HAURKUNING</div>
+            <div class="header4">Dusun Kaliwon, Kecamatan Nusaherang, Kabupaten Kuningan, Jawa Barat</div>
         </div>
     </div>
 
@@ -48,9 +51,5 @@
     <h1>Laporan Absensi</h1>
     <p>Isi laporan Anda...</p>
 
-    <!-- Footer -->
-    <div class="footer">
-        Line Sepanjang Dokumen - Ini adalah footer dokumen Anda
-    </div>
 </body>
 </html>
