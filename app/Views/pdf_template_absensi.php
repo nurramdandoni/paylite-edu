ed99d24f1fe75b3404f333857ccfefa4feef1cdb
Doni Nurramdan <nurramdandoni@gmail.com>
2023-09-04 13:19:38 +0700

    missing table isi col header tanggal fix colspan tanggal 15

--- app/Views/pdf_template_absensi.php
+++ app/Views/pdf_template_absensi.php
@@ -75,12 +75,24 @@
             <td rowspan="2">No</td>
             <td rowspan="2">NISN</td>
             <td rowspan="2">Nama Peserta Didik</td>
-            <td colspan="3">Tanggal</td>
+            <td colspan="15">Tanggal</td>
         </tr>
         <tr>
             <td>1</td>
             <td>2</td>
             <td>3</td>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+            <td>7</td>
+            <td>8</td>
+            <td>9</td>
+            <td>10</td>
+            <td>11</td>
+            <td>12</td>
+            <td>13</td>
+            <td>14</td>
+            <td>15</td>
         </tr>
         
     </table>
