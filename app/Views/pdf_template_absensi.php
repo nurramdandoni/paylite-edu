62e2567514a096c306cb7837b25023da8fce2b9a
Doni Nurramdan <nurramdandoni@gmail.com>
2023-09-04 11:49:13 +0700

    missing table

--- app/Views/pdf_template_absensi.php
+++ app/Views/pdf_template_absensi.php
@@ -54,10 +54,10 @@
             </tr>
         </table>
     </div>
-    <div style="text-align:center;margin-top:10px;">
+    <div style="text-align:center;margin-top:10px;font-size:16px;">
         <!-- Isi laporan Anda di sini -->
         <div style="margin-top:10px;">Rekapitulasi Absensi</div>
-        <div style="margin-top:0px;">Tahun Ajaran 2023 - Ganjil - <?= $anjir; ?></div>
+        <div style="margin-top:0px;">Tahun Ajaran 2023 - Ganjil</div>
     </div>
     <table border="0" style="margin-top:5px;font-size:11px;">
         <tr>
