0c3d7176385e3d392b65caaad868ab8af4b6e7fe
Doni Nurramdan <nurramdandoni@gmail.com>
2023-09-06 13:09:18 +0700

    add title header

--- app/Views/edu_absensiRekapitulasiKelas.php
+++ app/Views/edu_absensiRekapitulasiKelas.php
@@ -5,7 +5,7 @@
         <div class="col-12">
           <div class="card mb-4">
             <div class="card-header pb-0">
-              <h6>Data Absensi Rekapitulasi ->  <span id="titleKelas"></span> - Tahun Ajaran <span id="titleTahunAjaran"></span> </h6>
+              <h6>Data Absensi Rekapitulasi Mata Ajar ->  <span id="titleKelas"></span> - Tahun Ajaran <span id="titleTahunAjaran"></span> </h6>
             </div>
             <div class="card-body px-0 pt-0 pb-2">
               <div class="table-responsive p-0">
