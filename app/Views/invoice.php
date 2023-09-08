fdd7e74c4deabe894a4c46b3bd45ab5262a0c32c
Doni Nurramdan <nurramdandoni@gmail.com>
2023-09-08 15:26:19 +0700

    change favicon head

--- app/Views/invoice.php
+++ app/Views/invoice.php
@@ -1,6 +1,17 @@
-<html>
-	<head>
-    <link rel="icon" href="https://paylite.co.id/assets/img/favicon.png">
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="https://paylite.co.id/assets/img/favicon.png">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <title>Paylite Edu | Laporan Absensi</title>
 	</head>
 	<body>
         <div style="text-align:center;">
