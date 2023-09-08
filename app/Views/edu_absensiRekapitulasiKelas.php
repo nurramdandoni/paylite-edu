a9da8ba1c7c5876df1f39903b5f51de45a192d02
Doni Nurramdan <nurramdandoni@gmail.com>
2023-09-08 11:00:14 +0700

    param report periode col md

--- app/Views/edu_absensiRekapitulasiKelas.php
+++ app/Views/edu_absensiRekapitulasiKelas.php
@@ -21,13 +21,24 @@ $Stringtahun = $tahun;
                 <div id="contentFormInputEdit" class="row">
                   <div class="col-md-8"></div>
                   <div class="form-group col-md-4">
-                    <label for="bulan">Bulan</label>
-                    <select class="form-control " id="DKBulan">
-                      <option value="01">Januari</option>
-                    </select>
                     <label for="tahun">Tahun</label>
                     <select class="form-control " id="DKTahun">
                       <option value="2023">2023</option>
+                    </select>
+                    <label for="bulan">Bulan</label>
+                    <select class="form-control " id="DKBulan">
+                      <option value="01">Januari</option>
+                      <option value="02">Februari</option>
+                      <option value="03">Maret</option>
+                      <option value="04">April</option>
+                      <option value="05">Mei</option>
+                      <option value="06">Juni</option>
+                      <option value="07">Juli</option>
+                      <option value="08">Agustus</option>
+                      <option value="09">September</option>
+                      <option value="10">Oktober</option>
+                      <option value="11">November</option>
+                      <option value="12">Desember</option>
                     </select>
                   </div>
                 </div>
