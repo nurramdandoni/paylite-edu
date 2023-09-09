478254523127f05541d1e8a53f80d65979a931eb
Doni Nurramdan <nurramdandoni@gmail.com>
2023-09-09 21:23:07 +0700

    add meta monetag verify

--- app/Views/layout/page_edu_layout.php
+++ app/Views/layout/page_edu_layout.php
@@ -6,6 +6,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
+
+  <meta name="monetag" content="9b722102cb3a5226a7cf2d100971a273">
+  
   <link rel="icon" href="https://paylite.co.id/assets/img/favicon.png">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
     integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
