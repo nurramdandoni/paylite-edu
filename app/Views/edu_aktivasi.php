ef3c85542d85d2791a3e32a05424ba5a4ab47ca0
Doni Nurramdan <nurramdandoni@gmail.com>
2023-09-11 09:53:29 +0700

    visit ads with button

--- app/Views/edu_aktivasi.php
+++ app/Views/edu_aktivasi.php
@@ -1,10 +1,31 @@
 <?= $this->extend('layout/page_edu_layout') ?>
 
 <?= $this->section('content') ?>
-<iframe src="//whulsaux.com/4/6311694" frameborder="0" style="width: 100%; height: 100vh;"></iframe>
+<!-- <iframe src="//whulsaux.com/4/6311694" frameborder="0" style="width: 100%; height: 100vh;"></iframe> -->
+<button onclick="visitAds()">Visit</button>
 
 
 <script>
+  function visitAds(){
+
+    var link = "//whulsaux.com/4/6311694";
+    var newTab = window.open(link, '_blank');
+  
+    if (newTab) {
+      newTab.onload = function() {
+        if (newTab.document && newTab.document.documentElement) {
+          if (newTab.document.documentElement.innerHTML.includes("404 Not Found")) {
+            console.log("Tautan menghasilkan error 404.");
+          } else {
+            console.log("Tautan berhasil dimuat tanpa error 404.");
+          }
+        }
+      };
+    } else {
+      console.log("Tidak dapat membuka tautan di tab baru. Pastikan browser Anda mengizinkan jendela pop-up.");
+    }
+  }
+
         $("#liDash").html("Aktivasi");
         $("#liDash2").html("Aktivasi");
         $("#dashboard").removeClass("active");
