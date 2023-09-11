401f86218d5c1fc654aa13860fecf5efd725cc8a
Doni Nurramdan <nurramdandoni@gmail.com>
2023-09-11 09:45:18 +0700

    add ads in iframe full screen

--- app/Views/edu_aktivasi.php
+++ app/Views/edu_aktivasi.php
@@ -1,7 +1,7 @@
 <?= $this->extend('layout/page_edu_layout') ?>
 
 <?= $this->section('content') ?>
-<iframe src="//whulsaux.com/4/6311694" frameborder="0"></iframe>
+<iframe width="100%" height="100%" src="//whulsaux.com/4/6311694" frameborder="0"></iframe>
 
 <script>
         $("#liDash").html("Aktivasi");
